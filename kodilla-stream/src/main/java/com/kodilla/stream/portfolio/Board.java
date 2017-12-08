17db7b78b0c349c709f00ae9da82efdebc5d1ad9
mpopk <m.popkiewicz@gmail.com>
2017-12-08 17:16:58 +0100

    Portfolio

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/portfolio/Board.java
@@ -0,0 +1,32 @@
+package com.kodilla.stream.portfolio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+    private final List<TaskList> taskLists = new ArrayList<>();
+    private final String name;
+
+    public Board(final String name) {
+        this.name = name;
+    }
+
+    public void addTaskList(TaskList taskList) {
+        taskLists.add(taskList);
+    }
+
+    public boolean removeTaskList(TaskList taskList) {
+        return taskList.remove(taskList);
+    }
+
+    public String getName() {
+        return name;
+
+        @Override public String toString ( ) {
+            return "Board{" + "\n" + "name='" + name + '\'' + ",\n" + "taskLists=" + taskLists + "\n" + '}';
+        }
+    }
+}
+
+
+
