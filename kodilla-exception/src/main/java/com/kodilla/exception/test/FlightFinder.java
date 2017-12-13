d2467accf8a90dff36e348ddfce2e5c8a72f3f08
mpopk <m.popkiewicz@gmail.com>
2017-12-13 19:01:03 +0100

    Flight

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -7,24 +7,37 @@ import java.util.Map;
 public class FlightFinder {
 
 
-FlightFinder flightFinder = new FlightFinder();
-            private Map<String, Boolean> flights(){
-            Map<String, Boolean> flightSchedule = new HashMap<>();
+    FlightFinder flightFinder = new FlightFinder();
 
-            flightSchedule.put("WAW", true);
-            flightSchedule.put("MAD", true);
-            flightSchedule.put("LAX", false);
-            flightSchedule.put("LDN", true);
+    private Map<String, Boolean> flights() {
+        Map<String, Boolean> flightSchedule = new HashMap<>();
 
-            return flightSchedule;
+        flightSchedule.put("WAW", true);
+        flightSchedule.put("MAD", true);
+        flightSchedule.put("LAX", false);
+        flightSchedule.put("LDN", true);
+
+        return flightSchedule;
+    }
+
+
+    boolean findFilght(Flight flight) throws RouteNotFoundException {
+
+        for (Map.Entry<String, Boolean> entry : filghtSchedule.entrySet()) {
+            if (flight.getDepartureAirport().equals(entry.getKey())) {
+                System.out.println("Outbound flight found");
+            }
+            if (flight.getArrivalAirport().equals(entry.getKey())) {
+                System.out.println("Destination found");
+            }
+            if (boolean
+            ( flight.getArrivalAirport() != entry.getKey() || flight.getDepartureAirport()  != entry.getKey())  = true)
+            {
+                throw new RouteNotFoundException;
+            }
         }
-
-
-        boolean findFilght(Flight flight) throws RouteNotFoundException {
-            if (boolean flights.containsKey("WAW") = false;
-            if (boolean flights.containsKey("MAD") = false;
-            if (boolean flights.containsKey("LDN") = false;
-        }
+    }
+}
 
 
 
@@ -32,4 +45,4 @@ FlightFinder flightFinder = new FlightFinder();
     //informujący o tym czy można na dane lotnisko polecieć. Przykład: Map<String, Boolean>.
       //      c) logika powinna wyszukiwać loty w mapie.
     //Jeżeli dane lotnisko nie będzie istnieć w mapie, należy rzucić wyjątkiem RouteNotFoundException.
-}
+
